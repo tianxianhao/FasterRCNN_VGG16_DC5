def806d5f9fa85fa5f3143db045199038ee21643
tianxianhao <tianxianhao2019@email.szu.edu.cn>
2021-10-31 10:20:53 +0800

    Add files via upload

--- faster_rcnn_v16_fpn_1x_coco.py
+++ faster_rcnn_v16_fpn_1x_coco.py
@@ -2,6 +2,7 @@ _base_ = [
     '../_base_/datasets/coco_detection.py',
     '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
 ]
+# model settings
 model = dict(
     type='FasterRCNN',
     backbone=dict(
@@ -9,20 +10,25 @@ model = dict(
         depth=16,
         num_stages=4,
         dilations=(1, 1, 1, 1),
-        out_indices=(3,),
+        out_indices=(0,1,2,3,),
         frozen_stages=-1,
         bn_eval=True,
         with_bn=True,
         pretrained ='torchvision://vgg16_bn',),
+    neck=dict(
+        type='FPN',
+        in_channels=[64, 128, 256, 512],
+        out_channels=64,
+        num_outs=5),
     rpn_head=dict(
         type='RPNHead',
-        in_channels=512,  #channel changed
-        feat_channels=512,  #channel changed
+        in_channels=64,
+        feat_channels=64,
         anchor_generator=dict(
             type='AnchorGenerator',
-            scales=[2, 4, 8, 16, 32],
+            scales=[8],
             ratios=[0.5, 1.0, 2.0],
-            strides=[16]),
+            strides=[2, 4, 8, 16, 32]),
         bbox_coder=dict(
             type='DeltaXYWHBBoxCoder',
             target_means=[.0, .0, .0, .0],
@@ -35,11 +41,11 @@ model = dict(
         bbox_roi_extractor=dict(
             type='SingleRoIExtractor',
             roi_layer=dict(type='RoIAlign', output_size=7, sampling_ratio=0),
-            out_channels=512, #channel changed
-            featmap_strides=[16]),
+            out_channels=64,
+            featmap_strides=[2, 4, 8, 16]),
         bbox_head=dict(
             type='Shared2FCBBoxHead',
-            in_channels=512, #channel changed
+            in_channels=64,
             fc_out_channels=1024,
             roi_feat_size=7,
             num_classes=80,
@@ -67,12 +73,12 @@ model = dict(
                 pos_fraction=0.5,
                 neg_pos_ub=-1,
                 add_gt_as_proposals=False),
-            allowed_border=0,
+            allowed_border=-1,
             pos_weight=-1,
             debug=False),
         rpn_proposal=dict(
-            nms_pre=12000,
-            max_per_img=2000,
+            nms_pre=2000,
+            max_per_img=1000,
             nms=dict(type='nms', iou_threshold=0.7),
             min_bbox_size=0),
         rcnn=dict(
@@ -93,19 +99,15 @@ model = dict(
             debug=False)),
     test_cfg=dict(
         rpn=dict(
-            nms=dict(type='nms', iou_threshold=0.7),
-            nms_pre=6000,
+            nms_pre=1000,
             max_per_img=1000,
+            nms=dict(type='nms', iou_threshold=0.7),
             min_bbox_size=0),
         rcnn=dict(
             score_thr=0.05,
             nms=dict(type='nms', iou_threshold=0.5),
-            max_per_img=100)))
-work_dir = '../../work_dir/coco/frcnn/vgg16/1x/'
-optimizer = dict(type='SGD', lr=2e-3, momentum=0.9, weight_decay=5e-4)
-optimizer_config = dict(_delete_=True)
-custom_hooks = [
-    dict(type='NumClassCheckHook'),
-    dict(type='CheckInvalidLossHook', interval=50, priority='VERY_LOW')
-]
-find_unused_parameters =True
+            max_per_img=100)
+        # soft-nms is also supported for rcnn testing
+        # e.g., nms=dict(type='soft_nms', iou_threshold=0.5, min_score=0.05)
+    ))
+work_dir = '../../work_dirs/coco/frcnn/r50_1x/'
